2700c4a9b86f9a1f8d9bf6e3e6e64dbed03ab107
Devraj <devrajmandal5650@gmail.com>
2025-08-24 00:53:44 +0530

    Incremental changes

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,3 +1,51 @@
+import { NavLink } from "react-router-dom";
+
 export const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <>
+      <header className="section-navbar">
+        <section className="top-txt">
+          <div className="head container">
+            <div className="head_txt">
+              <p>Get membership, 30-day return or refund guarantee</p>
+            </div>
+            <div className="sing_in_up">
+              <NavLink to="#">SIGN IN</NavLink>
+              <NavLink to="#">SIGN UP</NavLink>
+            </div>
+          </div>
+        </section>
+
+        <div className="container">
+          <div className="navbar-brand">
+            <NavLink to="index">
+              <p>Netflix</p>
+            </NavLink>
+          </div>
+          <nav className="navbar">
+            <ul>
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="movie" className="nav-link">
+                  Movies
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="contact" className="nav-link">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
 };
